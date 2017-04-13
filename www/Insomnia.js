f161a60cea2c5dd12d837866ca33789e0afc71e0
Benjamin West <mrgenixus@gmail.com>
2017-04-13 15:21:00 -0600

    I prefer this style

--- www/Insomnia.js
+++ www/Insomnia.js
@@ -1,21 +1,16 @@
-function Insomnia() {
-}
-
-Insomnia.prototype.keepAwake = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "Insomnia", "keepAwake", []);
-};
-
-Insomnia.prototype.allowSleepAgain = function (successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, "Insomnia", "allowSleepAgain", []);
-};
-
-Insomnia.install = function () {
+cordova.addConstructor(function () {
   if (!window.plugins) {
     window.plugins = {};
   }
 
-  window.plugins.insomnia = new Insomnia();
+  window.plugins.insomnia = {
+    keepAwake: function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Insomnia", "keepAwake", []);
+    },
+    
+    allowSleepAgain: function (successCallback, errorCallback) {
+      cordova.exec(successCallback, errorCallback, "Insomnia", "allowSleepAgain", []);
+    }
+  };
   return window.plugins.insomnia;
-};
-
-cordova.addConstructor(Insomnia.install);
+});
